c4aedaef3bc7d3fabcc51e47afc7bac861ea36b4
nahuelherrera09 <naherrera09@gmail.com>
2021-10-01 12:42:40 -0300

    primeras funciones

--- js/index.js
+++ js/index.js
@@ -9,17 +9,54 @@ document.addEventListener('DOMContentLoaded', function(){
     const tableLujos = document.getElementById('tableLujos');
     const alert = document.getElementById('alert');
     const form = document.getElementsByClassName('form')
+    const removeBtn = document.getElementsByClassName('removeBtn')
 
     function addGasto(){
-        if(categoria.value === '1'){
+        if(descripcion.value === "" || gasto.value === ""){
+            alert.classList.remove("d-none");
+            alert.innerText = "No ingresaste gasto/descripción"
+        }
+        else if(categoria.value === '1'){
             const rowGastosfijos = tableGastosFijos.insertRow();
             rowGastosfijos.innerHTML = `
             <td>${gasto.value}</td>
             <td>${descripcion.value}</td>
-            <td> <button class="btn btn-danger ml-1">
+            <td> <button class="btn btn-danger ml-1 removeBtn">
+                <i class="fa fa-trash"></i> </td> 
+            `
+        } else if(categoria.value === '2'){
+            const rowComida = tableComida.insertRow();
+            rowComida.innerHTML = `
+            <td>${gasto.value}</td>
+            <td>${descripcion.value}</td>
+            <td> <button class="btn btn-danger ml-1 removeBtn">
+                <i class="fa fa-trash"></i> </td> 
+            `
+        }else if(categoria.value === '3'){
+            const rowTransporte = tableTransporte.insertRow();
+            rowTransporte.innerHTML = `
+            <td>${gasto.value}</td>
+            <td>${descripcion.value}</td>
+            <td> <button class="btn btn-danger ml-1 removeBtn">
+                <i class="fa fa-trash"></i> </td> 
+            `
+        }else{
+
+            const rowLujos = tableLujos.insertRow();
+            rowLujos.innerHTML = `
+            <td>${gasto.value}</td>
+            <td>${descripcion.value}</td>
+            <td> <button class="btn btn-danger ml-1 removeBtn">
                 <i class="fa fa-trash"></i> </td> 
             `
         }
+
+    }
+
+
+    function removeGasto(){
+        
+
     }
 
 
